c0641c40bf86c7c1583e449c5c30997c99f76faa
antonio vazquez <antonio.vazquez@axity.com>
2019-07-24 16:28:21 -0500

    se agrega codigo para fcm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -12,8 +15,35 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FCM,
+    private router: Router
   ) {
+    // obtener token FCM
+    this.fcm.getToken().then(token => {
+      console.log(token);
+    });
+
+    // actualizar token
+    this.fcm.onTokenRefresh().subscribe(token => {
+      console.log(token);
+    });
+
+    // recibir notificaciones
+    this.fcm.onNotification().subscribe(data => {
+      console.log(data);
+      if (data.wasTapped) {
+        console.log('Received in background');
+        this.router.navigate([data.landing_page, data.price]);
+      } else {
+        console.log('Received in foreground');
+        this.router.navigate([data.landing_page, data.price]);
+      }
+    });
+
+    // suscripcion a topico
+    this.fcm.subscribeToTopic('liveaxity');
+
     this.initializeApp();
   }
 
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+import { ActivatedRoute } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -14,4 +15,8 @@ import { Tab3Page } from './tab3.page';
   ],
   declarations: [Tab3Page]
 })
-export class Tab3PageModule {}
+export class Tab3PageModule {
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+}
